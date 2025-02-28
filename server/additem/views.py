cc7c52a9f7ffc72ba32ffb1ab569ac25be4c4feb
Oliver Quiroz <oliver.emmanuel.quiroz@gmail.com>
2025-02-28 16:35:42 +0800

    Merge edit of llei and arki

--- server/additem/views.py
+++ server/additem/views.py
@@ -7,6 +7,10 @@ from .serializers import ItemSerializer
 
 @api_view(['GET', 'POST'])
 def items_view(request):
+    """
+    GET -> Returns a list of all Items
+    POST -> Creates a new Item
+    """
     if request.method == 'GET':
         items = Item.objects.all().order_by('id')
         serializer = ItemSerializer(items, many=True)
@@ -16,10 +20,45 @@ def items_view(request):
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid():
             item = serializer.save()
+            # If item has a qr_code field (ImageField), return its URL
             qr_code_url = item.qr_code.url if item.qr_code else None
-            return Response({
-                "message": "Item added successfully",
-                "qr_code": qr_code_url
-            }, status=status.HTTP_201_CREATED)
-
+            return Response(
+                {
+                    "message": "Item added successfully",
+                    "qr_code": qr_code_url
+                },
+                status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def item_detail_view(request, pk):
+    """
+    GET -> Retrieve a single item
+    PUT -> Update an existing item
+    DELETE -> Delete the item
+    """
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response({"error": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    # Handle each method
+    if request.method == 'GET':
+        serializer = ItemSerializer(item)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = ItemSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response(
+            {"message": "Item deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT
+        )
